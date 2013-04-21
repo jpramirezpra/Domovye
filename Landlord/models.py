861731d38de5c6aa725a88cbf3a0042d9adffd88
Jose Pablo Ramirez <jp.ramirezpra@gmail.com>
2013-04-21 01:19:26 -0500

    kept working on models.py and template static files

--- Landlord/models.py
+++ Landlord/models.py
@@ -2,27 +2,60 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Landlord(User):
-
+	phonenumber = 
+	email =
+	website = 
+	stripe_info = 
 
 	def __unicode__(self):
 		return "Lanlord: "+self.username
 
 class Tenant(User):
+	phonenumber = 
+	email =
+	website = 
+	stripe_info = 
 
 	def __unicode__(self):
 		return "Tenant: "+self.username
 
 class Property(models.Model):
-
+	address = 
+	renters = 
+	owner = 
+	work_orders
+	expenses
 	
 	def __unicode__(self):
-			return self.title
+			return self.
 
 	class Meta:
-		verbose_name_plural = "stories"
+		verbose_name_plural = "properties"
 
-class Address(models.Model):
+class Lease(models.Model):
+	BILL_CYCLE_CHOICES = (
+		('D', 'Daily'),
+		('W', 'Weekly'),
+		('M', 'Monthly'),
+		)
+	landlord = 
+	tennents = 
+	prop = 
+	sign_date = 
+	end_date = 
+	deposit_amt =
+	bill_cylce = models.CharField(max_length=1,
+								choices=BILL_CYCLE_CHOICES,
+								default='D')
+	premium =
+	created_at =
+	modifified_at =
+	#photo or File of Contract
+
+
 
 	def __unicode__(self):
-		return self.t
+
+
+
 
